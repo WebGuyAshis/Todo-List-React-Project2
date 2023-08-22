9085b4738fd0e59681a030a8cecab069de20c57c
Ashis <developer.ashis99@gmail.com>
2023-08-23 04:46:12 +0530

    Added Utils Bar for DashBoard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Dashboard.styles.css'
 import DashNav from "./DashNav";
+import DashUtils from "./DashUtils";
 
 const Dashboard = () =>{
     return(
         <div className="dashboard-container"> 
             <DashNav/>
+            <DashUtils/>
         </div>
     )
 }
