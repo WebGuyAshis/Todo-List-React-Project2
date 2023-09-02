52838516f2c4021b5becb83eb609db431ede071f
Ashis <developer.ashis99@gmail.com>
2023-09-02 23:20:15 +0530

    Moved the Data Fetching through API to APP.JS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import DashboardPage from "./components/DashboardPage";
 import Footer from "./components/Footer";
+import React from "react";
 
+const FetchedContext = createContext();
 function App() {
+  const [tasks, setTasks] = useState();
+
+  const setDataToLocalStorage = (data) => {
+    localStorage.setItem("tasks", JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
+        setTasks(data);
+        setDataToLocalStorage(data);
+      } catch (error) {
+        console.log("Error Fetching Tasks!", error);
+      }
+    };
+
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    } else {
+      fetchData();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      {/* <Home/> */}
-      <Router>
-        <Routes>
-        <Route path="/" element= {<Home/>}/>
-        <Route path="/dashboard" element={<DashboardPage/>} />
-        </Routes>
-      </Router>
-      <Footer/>
-    </div>
+    <FetchedContext.Provider value={{ tasks, setTasks }}>
+      <div className="App">
+        {/* <Home/> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
+    </FetchedContext.Provider>
   );
 }
 
 export default App;
+export { FetchedContext };
--- src/components/DashboardPage/TasksContainer/TasksContainer.jsx
+++ src/components/DashboardPage/TasksContainer/TasksContainer.jsx
@@ -1,35 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./TasksContainer.styles.css";
 import { Link } from "react-router-dom";
 import plus from "../../../assets/svg/plus.svg";
 import Task from "../Task";
+import { FetchedContext } from "../../../App";
+
 
 const TasksContainer = () => {
-  const [tasks, setTasks] = useState([]);
-
-  const setDataToLocalStorage = (data) => {
-    localStorage.setItem('tasks', JSON.stringify(data));
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-        const data = await response.json();
-        setTasks(data);
-        setDataToLocalStorage(data);
-      } catch (error) {
-        console.log("Error Fetching Tasks!", error);
-      }
-    };
-
-    const storedTasks = localStorage.getItem('tasks');
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    } else {
-      fetchData();
-    }
-  }, []);
+  const {tasks, setTasks} = useContext(FetchedContext);
 
   const [isCompletedTab, setIsCompletedTab] = useState(false);
   const showPending = () => {
